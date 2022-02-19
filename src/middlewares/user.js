a8c337ca8a0d5652deb790373956d08e3315cfa8
Luis Antonio <luis.developer.ac@gmail.com>
2022-02-18 21:33:07 -0500

    refactor(middlewares): export error message

--- src/middlewares/user.js
+++ src/middlewares/user.js
@@ -1,9 +1,12 @@
-import { isAdmin } from '../constants/roles.js';
+import { isAdmin } from "../constants/roles.js";
+
+export const ERROR_MESSAGE =
+  "Você não possui permissão para executar essa operação.";
 
 // Valida se usuário é admin para realizar alguma ação que necessita de permissão
 export const isAdminMiddleware = (data) => {
   if (!isAdmin(data.user)) {
-    throw new Error('Você não possui permissão para executar essa operação.');
+    throw new Error(ERROR_MESSAGE);
   }
 
   return data;
